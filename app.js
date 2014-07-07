799cb2f1b45e31fc44121061886be6d6084e76df
kimlongngin <nginkimlong@gmail.com>
2014-07-07 13:41:53 +0700

    My first change

--- app.js
+++ app.js
@@ -1,110 +1,112 @@
 
+/*
+    app.js Description (23/05/2014):
+
+    Des: app.js is a file can involve all action throws it.
+        this file was created when we crete new project directory combine node.js with 
+        express framework .
+        Express framework using verson :4.0.0 
+    Object :
+        1. Upload file using connect-multiparty npm module beside express framework
+           so we must install it by commandline : sudo npn install connect-multiparty 
+        2. Create countdown in indexview 
+        3. Upload file in homepage views
+        .......
+        .......
+    controller : 
+        1. Index.jade : Control on countdown, retrieve answers from member, gift description
+        2. home.jade  : allow member upload file to challeng new generation
+        3. Login URL  : allow member upload and redirect to clientHome, client Activated 
+        .......
+        .......
+*/
 
 var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var app = express();
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var app = express();
+var bodyParser     = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/user');
-
-
-var format = require('util').format;
+var memberRoutes=require('./routes/member');
+var challengRoutes=require('./routes/challenge');
 var http = require('http');
-var fs = require('fs');
-/*var challengizProvider = require('./challengiz');*/
+var fs=require('fs');
+var sio = require('socket.io');
+var reload = require('reload');
+app.listen(8080);
+var server = http.createServer(app);
 
-// view engine setup
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(favicon());
 app.use(bodyParser());// pull information from html in POST
 app.use(logger('dev'));
-           
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser({keepExtensions: true,uploadDir: __dirname +'/ temp' })); 
-app.use(methodOverride()); 
-// Routes  by get Method#
 
+//app.use(require(methodOverride());
+app.use(bodyParser({keepExtensions: true,uploadDir: __dirname +'/ temp' })); 
+app.use(bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+// Routes  by get Method#
 app.get('/', routes.index);
 app.get('/home', routes.home);
 app.get('/register', routes.register);
-app.get('/prochaindefiHome', routes.prochaindefiHome);
-app.get('/prochaindefiActivated',routes.prochaindefiActivated);
-app.get('/prochaindefiLegagnant', routes.prochaindefiLegagnant);
+app.get('/clientHome', routes.clientHome);
+app.get('/clientActivated', routes.clientActivated);
+app.get('/second', routes.second); // second page after home page countdown timer
+
+app.get('/sendNumber', memberRoutes.AddnewQuizzNumber);
+
 
 // Request by Post Method 
 app.post('/register', routes.registers);
-app.post('/prochaindefiHome',routes.prochaindefiHome);
-
 app.post('/login', routes.mylogin); // Login on home page  2014-05-08
-//app.post('/upload', routes.uploadFile);// Upload file 2014-05-08
-
-app.post('/upload', function(req,res){
-    console.log('Start uploading...')
-    app.use(bodyParser({
-    keepExtensions: true,
-    uploadDir: __dirname +'/ temp' })); 
-
-    //get the file name
-    var filename=req.file.file.name;
-    console.log(filename);
-
-    var extensionAllowed=[".docx","doc","png","jpg","jpeg"];
-    var maxSizeOfFile=100;
-    var msg="";
-    var i = filename.lastIndexOf('.');
-    // get the temporary location of the file
-    var tmp_path = req.files.file.path;
-    console.log(tmp_path);
-
-    // set where the file should actually exists - in this case it is in the "images" directory
-    var target_path = __dirname +'/Upload/' + filename; 
-    var file_extension= (i < 0) ? '' : filename.substr(i); 
-    if((file_extension in oc(extensionAllowed))&&((req.files.file.size /1024 )< maxSizeOfFile)){ 
-    fs.rename(tmp_path, target_path, function(err) {
-    if (err) throw err; 
-    // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files 
-    fs.unlink(tmp_path, function() {
-    if (err) throw err;
-    });
-    });
-    msg="File uploaded sucessfully" 
-    }else{
-    // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files 
-    fs.unlink(tmp_path, function(err) {
-    if (err) throw err;
-    });
-    msg="File upload failed.File extension not allowed and size must be less than "+maxSizeOfFile; 
-    }
-    res.end(msg);
-    res.redirect('/prochaindefiHome');
-    
-});
-function oc(a){
-    var o = {};
-    for(var i=0;i<a.length;i++) {
-    o[a[i]]='';
-    }
-    return o; 
-}
 
 
+var multipart = require('connect-multiparty');// enctype='multipart/form-data'
+var multipartMiddleware = multipart(); 
+app.post('/myUpload',multipartMiddleware,routes.myUpload);
 
 
-/*++++++++++++++++++ The page login to register for client ++++++++++++++++++*/
+/*+++++++++++++++++++++++++++ Challeng Route Involve ++++++++++++++*/
+
+app.get('/challengPhoto',challengRoutes.ChallengePhoto);
+app.get('/challengeQuizz',challengRoutes.ChallengeQuizz);
+app.get('/challengeGoods',challengRoutes.ChallengeGoods);
+app.get('/challengeHistory', challengRoutes.ChallengeHistory)
+app.get('/view',challengRoutes.Views)
+app.get('/profileEdit',challengRoutes.profileEdits)
+app.get('/updateAdvertise', challengRoutes.UpdateAdvertise);
+app.post('/updateAdvertise', challengRoutes.UpdateChallengePost);
+app.post('/profileEdit', challengRoutes.profileEditClient);
+
+/*+++++++++++++++++++ Upload file ++++++++++++++++++++++++++++++*/
+
+var multipart = require('connect-multiparty');// enctype='multipart/form-data'
+var multipartMiddleware = multipart(); // Create new object from module multipart
 
 
+/*+++++++++++++++++++ Block Member Space ++++++++++++++++++++++++*/
+app.get('/memberHome', memberRoutes.memberHome);
+app.get('/memberAdvertise', memberRoutes.memberAdvertise);
+app.get('/memberspace', memberRoutes.memberspace);
+app.get('/myquizz',memberRoutes.myquizz);
+app.get('/newquizz', memberRoutes.newquizz);
+app.get('/challengiz',memberRoutes.Challengiz);
+app.get('/editQuizz', memberRoutes.EditQuizz);
+app.post('/editQuizz', memberRoutes.EditQuizzPost);
 
 
+app.post('/memberAdvertise',memberRoutes.memberAdvertiseDelUp);
+app.post('/memberspaceAdd', memberRoutes.memberspaceAdd);
+app.post('/myquizz',memberRoutes.myquizzDelUp);
+app.post('/newquizz', memberRoutes.AddnewQuizz);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -112,10 +114,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
-
-
-/// error handlers
-
+// error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -138,6 +137,25 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(8080)
+
+
+
+var io = sio.listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log("Wellcome!")
+    socket.on('user image', function (msg) {
+        //Received an image: broadcast to all
+        socket.broadcast.emit('user image', socket.nickname, msg);
+    });
+    socket.on('user message', function (msg) {/*See source code*/});
+    socket.on('nickname', function (msg) {/*See source code*/});
+});
+
+var countdown = 1000;
+setInterval(function() {
+  countdown--;
+  io.sockets.emit('timer', { countdown: countdown });
+}, 1000);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,101 +2,231 @@
 /*
  * GET home page.
  */
-/*var client_data = require('../challengiz'); */
-var express = require('express');
 
-var app = express();
-//Directory to upload file
-var uploadPath="upload";
-
-
-
-
-var mongodb = require('mongodb');
-var MONGODB_URI = 'mongodb://dbchallengiz:12345@ds037758.mongolab.com:37758/dbchallengiz?w=1';
-var db;
-var coll;
-var sas_origin_coll;
-var giftcoll;
+var con = require('./connection.js'); // Call file store connection module 
+con.colls();// Create connection and get all paramenter table 
+var express=require('express');
+var app=express();
+var open =require('open'); // using open npm module "sudo npm install open"
+var _=require('underscore'); // Using underscore.js module "sudo npm install underscore"
+var reload = require('reload');
+var http=require('http');
+var server = http.createServer(app);
+var session = require('express-session');
+var cookie = require('cookie-parser');
+var Cookies =require('cookies');
 
 
-// Initialize connection once
-mongodb.MongoClient.connect(MONGODB_URI, function(err, database) {
-  if(err) throw err;
-  db = database;
-  coll = db.collection('client'); 
-  sas_origin_coll = db.collection('sas_origines');
-  giftcoll=db.collection('gift');
-  console.log('connected');
-});
- 
-/*coll.find({}, function(err, docs) {
-    docs.each(function(err, doc) {
-      if(doc) {
-        console.log(doc)
-      }
-      else {
-        res.end();
-      }
-    });
-});*/
 
+/*+++++++++++++++++++++++++ General Block ++++++++++++++++++++*/
+var homecount=60;
+var homei=0;
+exports.home = function(req, res, files){
 
-var count=60;
-var i=0;
-exports.index = function(req, res){
-	i=i+1;
-	
-		coll.find().toArray(function(err, docs) {
-			if(err)
+		con.giftcoll.find({is_activated:0}).limit(2).toArray(function(err, gift_result) {
+			if (gift_result.length<=0)
 			{
-				console.log(err)
-			}else{
 			
-				if(req.method==='GET'){		
-						clearInterval();
-						setInterval(function(){
-							if(count <=0)
-							{
-							
-								clearInterval(count);
+				res.render('home', { title: 'home',countnum:homecount, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+			
+			}else{	
 
-							}else{
-								count--;
-								//console.log("Count:"+count);
-							}
-						},1000)
+				var gift1=gift_result[0].img;
+				var gift2=gift_result[1].img;
+				var gift_id1 = gift_result[0]._id;
+				var gift_id2 = gift_result[1]._id;
 
-					res.render('index', { title: 'index',countNum: count, dataclient:docs});
-				}
+				if(gift1<=0 || gift2<=0)
+				{
+
+					res.render('home', { title: 'home',countnum:homecount, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+				
+				}else{
+
+					if(homei<=0){	
+						var interval=setInterval(function(){
+						homecount--;
+						console.log(homecount);
+			
+						if(homecount<=0)
+						{
+							/*
+							giftcoll.update({_id: gift_id1 }, { $set: { "is_activated": 1 } }, function(error, callback){
+								if(error){
+									console.log("Update Error 1");
+								}else
+								{
+									console.log("Success Update 1");
+								}
+							});
+
+							giftcoll.update({_id: gift_id2}, { $set: { "is_activated": 1 } }, function(req,res,error, callback){
+								if(error){
+									console.log("Update Error2");
+								}else
+								{
+									console.log("Success Update 2");
+									res.redirect('/home');
+								}
+							});
+							*/
+
+							clearInterval(interval);
+							open('http://localhost:8080/second', function (err) {
+							  if (err) throw err;
+							});
+						}													
+
+					  },1000)
+					  res.render('home', { title: 'home',countnum:homecount, gift1:gift1, gift2:gift2});				
+					}else{
+						console.log("You run me again!");
+						console.log(homecount);
+						res.render('home', { title: 'home',countnum:homecount, gift1:gift1, gift2:gift2});				
+				
+					}
+
 			}
-		})
+		}
+	})	
 	
 
-};
+}
+
+/*+++++++++++++++++++ Call back function ++++++++++++++++++++++++*/
+	/*
+		console.log("Start callback...!");
+		var myCallback = function(data) {
+		  console.log('got data: '+data);
+		};
+		var usingItNow = function(callback) {
+		  callback('get it?');
+		};
+		usingItNow(myCallback);
+	*/
 
 
 
 
+var i = 0;
+var count=120;
+var indexi=0;
 
-exports.home = function(req, res){
-	coll.find().toArray(function(err, docs) {
+function final() { console.log('Done'); }
+
+
+exports.index = function(req, res){	
+	con.coll.find().toArray(function(err, docs) {
 		if(err)
 		{
 			console.log(err)
 		}else{
 		    console.dir(docs);
 			if(req.method==='GET'){	
-				res.render('home', { title: 'home',dataclient:docs});
-			}
+			con.giftcoll.find({is_activated:0}).limit(2).toArray(function(err, gift_result) {
+				if (gift_result.length<=0)
+				{
+					res.render('index', { title: 'index',dataclient:docs,countnum:count, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+				}else{	
+					var gift1=gift_result[0].img;
+					var gift2=gift_result[1].img;
+					var gift_id1 = gift_result[0]._id;
+					var gift_id2 = gift_result[1]._id;
+
+				if(gift1<=0 || gift2<=0)
+				{
+					res.render('index', { title: 'index',dataclient:docs,countnum:count, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+				}else{
+					if(indexi<=0){
+						var interval=setInterval(function(){
+							indexi=indexi+1;
+							count--;
+							if(count <=0)
+							{
+								clearInterval(interval,0);
+						/*+++++++++++ This module use for ooopen new page when time countdown till 0  +++++++++*/
+					 			open('http://localhost:8080/home', function (err) {
+								  if ( err ) throw err;
+								});
+
+								  setTimeout(function() {
+								    console.log('This will not run');
+								  }, 0);
+							}
+					    },1000)	
+					    res.render('index', { title: 'index',dataclient:docs,countnum:count, gift1:gift1, gift2:gift2});
+					}else{
+						 console.log("You run me again!");
+						 console.log(count);
+						 res.render('index', { title: 'index',dataclient:docs,countnum:count, gift1:gift1, gift2:gift2});
+					}
+
+				}
+
+			  }
+			})	
+		   }
 		}
-		//db.close();
+		
 	})
 };
 
+
+var mycount=60;
+var secondi=0;
+exports.second = function(req, res, files){
+	con.giftcoll.find({is_activated:0}).limit(2).toArray(function(err, gift_result) {
+			if (gift_result.length<=0)
+			{
+				res.render('second', { title: 'second',countnum:mycount, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+			}else{	
+				var gift1=gift_result[0].img;
+				var gift2=gift_result[1].img;
+				var gift_id1 = gift_result[0]._id;
+				var gift_id2 = gift_result[1]._id;
+				
+				if(gift1<=0 || gift2<=0)
+				{
+					res.render('second', { title: 'second',countnum:mycount, gift1:"no-gift-7.jpg", gift2:"no-gift-7.jpg"});		
+				}else{
+		
+					if(secondi<=0){
+				
+						var interval=setInterval(function(){
+							mycount--;
+							
+							if(mycount <=0)
+							{
+								clearInterval(interval);
+								console.log("You Reload");
+								reload(server,app);
+							}
+
+					  },1000)
+						secondi=secondi+1;
+					  	res.render('second', { title: 'second',countnum:mycount, gift1:gift1, gift2:gift2});
+					}else
+					{
+						console.log("You run me again!");
+						console.log(mycount);
+						res.render('second', { title: 'second',countnum:mycount, gift1:gift1, gift2:gift2});
+					}
+				}
+			}
+
+
+	});	
+};
+
+
+
+
+
+/*++++++++++++++++++++++++++ Block Client ++++++++++++++++++++++*/
+/*Get Client member when login to challengiz from client table*/
 exports.register = function(req, res){
 	if(req.method==='GET'){
-		sas_origin_coll.find().toArray(function(error, sas_result){
+		con.sas_origin_coll.find().toArray(function(error, sas_result){
 			if(error)
 			{
 				console.log(error)
@@ -109,18 +239,16 @@ exports.register = function(req, res){
 };
 
 
-  
-// this code should be executed when the client receives a message from the server.
+/*
+	1. Register Route: This route use for client create new 
+	   account to join in challengiz
+	2. Action: Only database and store in client table
 
-
-
-
+*/
 exports.registers = function(req, res)
 {
 
-
 	if(req.method ==='POST'){
-
 	  var txtorigine = req.body.txtname_orgine;
 	  var txtst = req.body.txtst;
 	  var sex =req.body.sex;
@@ -137,8 +265,7 @@ exports.registers = function(req, res)
 	  var txtcon_email =req.body.txtcon_email;
 
 	if(txtpwd == txt_con_pwd && txtemail==txtcon_email){
-
-		coll.save({
+		con.coll.save({
 	        society_name: txtorigine,
 		    siren: txtst,
 		    name: txtlastname,
@@ -152,9 +279,8 @@ exports.registers = function(req, res)
 		    city: txtcity,
 		    country: country_id,
 		    is_activated: 0,
-		    creation_date:new Date()
-
-
+		    creation_date:new Date(),
+		    pubtype:[]
 		  }, function( error, docs) {
 		        res.redirect('/')
 		  });
@@ -166,36 +292,84 @@ exports.registers = function(req, res)
  }
 }
 
-/*++++++++++++++++++++++ Get the gift show on home page +++++++++++++++++++++++++++*/
 
-exports.prochaindefiHome = function(req, res){
+/* 
+	1. clientHome Route: will occuring when user loing 
+	2. Action : 
+		2.1 clientHome's view will view gift picture 
+		2.2 clientHome's view will view coutdown timer on the view
+			to set challeng time to player.
+
+*/
+
+
+exports.clientHome = function(req, res)
+{
+	var cookies= new Cookies(req, res);
+	console.log("COOKIE VALUE:"+ cookies.get('IDLogin'));
 
 	if(req.method==='GET'){	
-		giftcoll.find().limit(2).toArray(function(error, gift_result){
+		con.giftcoll.find().limit(2).toArray(function(error, gift_result){
+			console.log("result:"+gift_result[0].creation_date);
 			var par1=gift_result[0].img;
 			var par2=gift_result[1].img;
 			if(error)
 			{
 				console.log(error)
 			}else{
-
 				console.log("Procahin Defi start");
 				console.log(gift_result);
-				res.render('prochaindefiHome', { title: 'Prochain Defi',gift1: par1, gift2: par2});
+				res.render('clientHome', { title: 'Prochain Defi',gift1: par1, gift2: par2});
 			}
+		})
+	}
+}
+
+/* 
+	1. clientActivated Route: will occuring when user clientHome's count down timer 
+							  coutn time equal 0 
+	2. Action : 
+		2.1 clientActivated's view will view gift picture 
+		2.2 clientActivated's view will view coutdown timer on the view
+			to set challeng time to player.
+		2.3 clientActivated's view will provider permission to player
+			can upload the gift's pictures to challeng
+			
+*/
+exports.clientActivated = function(req, res)
+{
+	var count=120;
+	if(req.method==='GET')
+	{
+		con.giftcoll.find().limit(2).toArray(function(error, gift_result){
+			var par1=gift_result[0].img;
+			var par2=gift_result[1].img;
+			
+			if(error)
+			{
+				console.log(error)
+			}else{
+				console.log("Procahin Defi start");
+				console.log(gift_result);
+
+				var interval=setInterval(function(){
+						count--;
+						if(count <=0)
+						{
+	
+							clearInterval(interval);
+						}													
+
+				  },1000)
+
+				res.render('clientActivated',{title:'clientActivated',countnum: count, gift1: par1, gift2: par2})
+			}
+
+
 		})
 	}
 
 }
-	
-function oc(a){
-	var o = {};
-	for(var i=0;i<a.length;i++) {
-	o[a[i]]='';
-	}
-	return o; 
-}
-
 
 
 // +++++++++++++++++ Login Module ++++++++++++++++++++++++
@@ -204,7 +378,7 @@ exports.indexheads = function(req, res)
 {
 	var txtusername = req.body.txtusername;
 	var txtpwd=req.body.txtpwd;
-	coll.find({email:txtusername, password:txtpwd}).toArray(function(err, docs) {
+	con.coll.find({email:txtusername, password:txtpwd}).toArray(function(err, docs) {
 		console.log(docs);
 		if(docs.length >0 )
 		{
@@ -216,37 +390,41 @@ exports.indexheads = function(req, res)
  
 // Prochain - Defi
 
-exports.prochaindefiActivated  = function(req, res)
+
+/*exports.prochaindefiActivated  = function(req, res)
 {
+	var count=120;
 	if(req.method==='GET')
 	{
-		giftcoll.find().limit(2).toArray(function(error, gift_result){
+		con.giftcoll.find().limit(2).toArray(function(error, gift_result){
 			var par1=gift_result[0].img;
 			var par2=gift_result[1].img;
+			
 			if(error)
 			{
 				console.log(error)
 			}else{
 				console.log("Procahin Defi start");
 				console.log(gift_result);
-				res.render('prochaindefiActivated',{title:'prochaindefiActivated',gift1: par1, gift2: par2})
+
+				var interval=setInterval(function(){
+						count--;
+						if(count <=0)
+						{
+	
+							clearInterval(interval);
+						}													
+
+				  },1000)
+
+				res.render('prochaindefiActivated',{title:'prochaindefiActivated',countnum: count, gift1: par1, gift2: par2})
 			}
+
+
 		})
 	}
-
-}
-
-/*exports.prochaindefiActivated = function(req, res,error)
-{
-	if(req.method ==='POST'){
-
-		 var txtorigine = req.body.txtname
-		 var uploadDir = __dirname + '/public/uploads/'
-		 console.log(txtorigine)
-		 console.log(uploadDir)
-	
-	}
 }*/
+
 
 exports.prochaindefiLegagnant  = function(req, res)
 {
@@ -257,136 +435,119 @@ exports.prochaindefiLegagnant  = function(req, res)
 } 
 
 
-
-
-// ++++++++++++++++++++++ Login ++++++++++++++++++
-exports.mylogin = function(req, res)
-{
-	console.log('Start Login');
-    var txtusername = req.body.txtusername;
-    var txtpwd=req.body.txtpwd;
-    console.log("mYname:"+txtusername+","+txtpwd)
-    coll.find({email:txtusername, password:txtpwd}).toArray(function(err, docs) {
-        console.log(docs);
-        if(docs.length >0 )
-        {
-            //res.render('prochaindefiHome', { title: 'Prochain Defi'});
-            res.redirect('/prochaindefiHome');
-        }
-    });
-}
-
-
-
-/*++++++++++++++++++++++ Upload Image +++++++++++++*/
-
-var formidable = require('formidable'),
-		c = require('config'),
-		fs = require('fs');
-
-exports.upload = function(req, res){
+/*
 	
-	var form = new formidable.IncomingForm();
-	
-	// Parse file.
-  form.parse(req, function(err, fields, files) {
-  	
-  	if(files.file){
-			
-			// Read file.			
-			fs.readFile(files.file.path, function(err, data){
+	myUpload Route: this route extend from home view when client click on 
+					button upload 
+	Action 		  : The view will upload all picture or gift to Upload folder
+					and will store the image name to gift table in our database
 
-		  	// Save file.
-				fs.writeFile(c.config.appPath + '/content/files/' + 
-					files.file.name, 
-					data, 
-					'utf8', 
-					function (err) {
-						if (err){
-							// throw err;
-							res.writeHead(200, {'content-type': 'text/plain'});
-							res.write(JSON.stringify({
-								isSucessful: false,
-								message: 'Something went wrong!'					
-							}));
-							res.end();
-						} else {
-							// Sucess.
-							res.writeHead(200, {'content-type': 'text/plain'});
-							res.write(JSON.stringify({
-								isSucessful: true,
-								message: 'File was saved!'
-							}));
-							res.end();
-						}
-				});
-			});
-		} else {
-			res.writeHead(200, {'content-type': 'text/plain'});
-			res.write(JSON.stringify({
-				isSucessful: false,
-				message: 'Did not receive any file!'					
-			}));
-			res.end();
-		}
-  });
-};
+*/
 
 
-
+var bodyParser= require('body-parser')
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
 var fs = require('fs');
-var path = require('path');
 
-	
-exports.uploadFile = function(req, res, files)
+exports.myUpload=function(req,res)
 {
-	// all environments
-	app.use(bodyParser({
-	keepExtensions: true,
-	uploadDir: __dirname +'/ temp' })); 
+	/*console.log(req.files.file.name);		
+	console.log(req.files.file.path);	
+	console.log("You are success!");*/
 
-	//get the file name
-	var filename=req.files.file;
-	console.log(filename);
-
-	var extensionAllowed=[".docx","doc","png","jpg","jpeg"];
-	var maxSizeOfFile=100;
+	var filename=req.files.file.name;	
+	var extensionAllowed=[".docx","doc",".png",".jpg",".jpeg",". gif"];
+	var maxSizeOfFile=1000;
 	var msg="";
 	var i = filename.lastIndexOf('.');
 	// get the temporary location of the file
 	var tmp_path = req.files.file.path;
-	console.log(tmp_path);
-
 	// set where the file should actually exists - in this case it is in the "images" directory
-	var target_path = __dirname +'/Upload/' + filename; 
-	var file_extension= (i < 0) ? '' : filename.substr(i); 
-	if((file_extension in oc(extensionAllowed))&&((req.files.file.size /1024 )< maxSizeOfFile)){ 
-	fs.rename(tmp_path, target_path, function(err) {
-	if (err) throw err; 
-	// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files 
-	fs.unlink(tmp_path, function() {
-	if (err) throw err;
-	});
-	});
-	msg="File uploaded sucessfully" 
+	/*var target_path = __dirname +'/Upload/' + req.files.file.name; */
+	var target_path = 'public/Upload/' + req.files.file.name;
+	console.log(target_path);
+	var file_extension= (i < 0) ? '' : filename.substr(i);
+	if((file_extension in oc(extensionAllowed))&&((req.files.file.size /1024 )< maxSizeOfFile)){
+		fs.rename(tmp_path, target_path, function(err) {
+			if (err) throw err;
+			// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+			fs.unlink(tmp_path, function() {
+				if (err) throw err;
+			});
+		});
+		msg="File uploaded sucessfully"
 	}else{
-	// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files 
+	
+	// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
 	fs.unlink(tmp_path, function(err) {
-	if (err) throw err;
+		if (err) throw err;
 	});
-	msg="File upload failed.File extension not allowed and size must be less than "+maxSizeOfFile; 
+		msg="File upload failed.File extension not allowed and size must be less than "+maxSizeOfFile;
 	}
 	res.end(msg);
-	res.redirect('/prochaindefiHome');
 
 }
 
 function oc(a){
 	var o = {};
 	for(var i=0;i<a.length;i++) {
-	o[a[i]]='';
+		o[a[i]]='';
 	}
-	return o; 
-}	
+	return o;
+}
+
+
+
+// ++++++++++++++++++++++ Login ++++++++++++++++++
+
+/*
+	mylogin Route: this route allow user or client make login 
+					to view or create new challengiz in our program 
+	Action 		 : check login and store session
+*/
+
+app.use(cookie());
+app.use(session({ secret: 'keyboard cat'}));
+
+exports.mylogin = function(req, res)
+{
+	
+
+	console.log('Start Login');
+    var txtusername = req.body.txtusername;
+    var txtpwd=req.body.txtpwd;
+    console.log("Session starting...");
+   
+
+    con.coll.find({email:txtusername, password:txtpwd}).toArray(function(err, docs) {
+        console.log("+++++++++++++++++++++++++++++");
+        console.log(docs);
+        if(docs.length >0 )
+        {
+           //res.render('prochaindefiHome', { title: 'Prochain Defi'});
+           res.cookie('EmailLogin', req.body.txtusername);
+           res.cookie('EmailPwd', req.body.txtpwd);
+           res.cookie('IDLogin', docs[0]._id)
+        
+
+           // res.session.txtusername = req.body.txtusername;
+           res.redirect('/clientHome/?val=1');
+          
+          // res.redirect('/clientHome/?val=1');
+        }else
+        {
+        	res.redirect('/clientHome/?val=0');
+        }
+    });
+
+}
+
+
+
+
+
+
+
 
 	
